9be67b85dbdf262f41ca0e9b18d694c9ecd63149
Aryan <ark.opash@gamil.com>
2026-03-19 12:23:52 +0530

    route and error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import authRoutes from "./routes/auth.router.js";
+import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
+
+// Global error handler
+app.use(errorHandler);
 
 export default app;
